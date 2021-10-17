334484bfc428ad83d70de1d6b05e780c70042d5f
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-10-17 23:26:48 +0530

    Add files via upload

---
+++ myjs2.js
@@ -0,0 +1,10 @@
+/*<script>
+    function Dispalytext()
+    {
+        document.getElementById("text2")=innerHtml="Welcome to day 2 session";
+    }
+</script>*/
+//js programming
+let StudentDetails= "Ravi";
+var name="raj";
+var  Name="ravi";
